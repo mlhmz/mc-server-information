43ace6654c5d1b5b947e2523a0dd6d28e3d39f5a
Malek Hamza <malek.hamza@gmx.de>
2023-08-29 19:13:26 +0200

    Add InformationPublisher with Redis Implementation

---
+++ server-information-producer/src/main/java/xyz/mlhmz/mcserverinformation/serverinformationproducer/publisher/InformationPublisher.java
@@ -0,0 +1,7 @@
+package xyz.mlhmz.mcserverinformation.serverinformationproducer.publisher;
+
+import xyz.mlhmz.mcserverinformation.serverinformationproducer.Information;
+
+public interface InformationPublisher {
+    void publish(Information information);
+}
---
+++ server-information-producer/src/main/java/xyz/mlhmz/mcserverinformation/serverinformationproducer/publisher/RedisInformationPublisher.java
@@ -0,0 +1,33 @@
+package xyz.mlhmz.mcserverinformation.serverinformationproducer.publisher;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import redis.clients.jedis.Jedis;
+import xyz.mlhmz.mcserverinformation.serverinformationproducer.Information;
+import xyz.mlhmz.mcserverinformation.serverinformationproducer.ServerInformationProducer;
+import xyz.mlhmz.mcserverinformation.serverinformationproducer.fetcher.InformationFetcher;
+
+import java.util.logging.Level;
+
+public class RedisInformationPublisher implements InformationPublisher {
+    private final Jedis client;
+    private final String channel;
+    private final ServerInformationProducer plugin;
+
+    public RedisInformationPublisher(Jedis client, String channel,
+                                     ServerInformationProducer plugin) {
+        this.client = client;
+        this.channel = channel;
+        this.plugin = plugin;
+    }
+
+    @Override
+    public void publish(Information information) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            client.publish(channel, objectMapper.writeValueAsString(information));
+        } catch (JsonProcessingException e) {
+            plugin.getLogger().log(Level.SEVERE, "The information couldn't be mapped to JSON.");
+        }
+    }
+}
