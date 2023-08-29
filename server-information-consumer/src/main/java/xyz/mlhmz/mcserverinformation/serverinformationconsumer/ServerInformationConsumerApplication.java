e29139252dc1699210964506ff58681b31eac712
Malek Hamza <malek.hamza@gmx.de>
2023-08-29 22:44:57 +0200

    Add Server Information Consumer Module to Monorepo

---
+++ server-information-consumer/src/main/java/xyz/mlhmz/mcserverinformation/serverinformationconsumer/ServerInformationConsumerApplication.java
@@ -0,0 +1,13 @@
+package xyz.mlhmz.mcserverinformation.serverinformationconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerInformationConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerInformationConsumerApplication.class, args);
+    }
+
+}
