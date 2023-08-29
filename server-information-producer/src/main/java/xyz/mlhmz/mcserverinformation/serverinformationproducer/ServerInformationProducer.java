82c481de35a7eca3e1270793dbcd9f8ade737126
Malek Hamza <malek.hamza@gmx.de>
2023-08-29 19:54:49 +0200

    Add a info and reload command and the main method that adds all up

--- server-information-producer/src/main/java/xyz/mlhmz/mcserverinformation/serverinformationproducer/ServerInformationProducer.java
+++ server-information-producer/src/main/java/xyz/mlhmz/mcserverinformation/serverinformationproducer/ServerInformationProducer.java
@@ -1,17 +1,63 @@
 package xyz.mlhmz.mcserverinformation.serverinformationproducer;
 
+import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitTask;
+import redis.clients.jedis.JedisPool;
+import xyz.mlhmz.mcserverinformation.serverinformationproducer.commands.ServerInformationProducerCommand;
+import xyz.mlhmz.mcserverinformation.serverinformationproducer.fetcher.BukkitServerInformationFetcher;
+import xyz.mlhmz.mcserverinformation.serverinformationproducer.publisher.RedisInformationPublisher;
+
+import java.util.Objects;
 
 public final class ServerInformationProducer extends JavaPlugin {
+    public static final String SIP_COMMAND_KEY = "sip";
+    private BukkitTask task;
+    @Getter
+    private String redisHost;
+    @Getter
+    private int redisPort;
+    @Getter
+    private String redisChannel;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        createCommands();
+        this.saveDefaultConfig();
+        executeInformationBukkitTask();
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
     }
+
+    public void reload() {
+        this.reloadConfig();
+        task.cancel();
+        executeInformationBukkitTask();
+    }
+
+    private void createCommands() {
+        Objects.requireNonNull(this.getCommand(SIP_COMMAND_KEY))
+                .setExecutor(new ServerInformationProducerCommand(this));
+    }
+
+    private void executeInformationBukkitTask() {
+        redisHost = this.getConfig().getString("redis.host", "localhost");
+        redisPort = this.getConfig().getInt("redis.port", 6379);
+        redisChannel = this.getConfig().getString("redis.channel", "server-info");
+        Runnable informationTaskExecution = () -> {
+            try (JedisPool jedisPool = new JedisPool(redisHost, redisPort)) {
+                InformationTask informationTask = new InformationTask(
+                        new BukkitServerInformationFetcher(this.getServer()),
+                        new RedisInformationPublisher(jedisPool.getResource(), redisChannel, this)
+                );
+                informationTask.execute();
+            }
+        };
+        task = this.getServer().getScheduler().runTaskTimer(this, informationTaskExecution, 1000, 1000);
+    }
+
 }
---
+++ server-information-producer/src/main/java/xyz/mlhmz/mcserverinformation/serverinformationproducer/commands/ServerInformationProducerCommand.java
@@ -0,0 +1,65 @@
+package xyz.mlhmz.mcserverinformation.serverinformationproducer.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+import xyz.mlhmz.mcserverinformation.serverinformationproducer.ServerInformationProducer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ServerInformationProducerCommand implements CommandExecutor {
+    private final ServerInformationProducer plugin;
+
+    public ServerInformationProducerCommand(ServerInformationProducer plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label,
+                             String[] args) {
+        Map<String, Runnable> subCommands = getSubCommands(sender);
+        String availableCommands = String.join("|", subCommands.keySet());
+        if (args[0] == null) {
+            sender.sendMessage(String.format("No args were provided. Please try again. Usage: /%s <%s>", label,
+                    availableCommands));
+            return false;
+        }
+        Runnable subCommand = subCommands.get(args[0]);
+        if (subCommand == null) {
+            sender.sendMessage(String.format("No command was found. Please try again. Usage: /%s <%s>", label,
+                    availableCommands));
+            return false;
+        }
+        subCommand.run();
+        return true;
+    }
+
+    private Map<String, Runnable> getSubCommands(CommandSender sender) {
+        Map<String, Runnable> subCommands = new HashMap<>();
+        subCommands.put("reload", () -> executeReloadCommand(sender));
+        subCommands.put("info", () -> executeInfoCommand(sender));
+        return subCommands;
+    }
+
+    private void executeReloadCommand(CommandSender sender) {
+        if (!sender.hasPermission("sip.reload")) {
+            sender.sendMessage("You have no permission to reload this plugin.");
+            return;
+        }
+        plugin.reload();
+    }
+
+    private void executeInfoCommand(CommandSender sender) {
+        if (!sender.hasPermission("sip.info")) {
+            sender.sendMessage("You have no permission to look at this plugins info.");
+            return;
+        }
+        sender.sendMessage("ServerInformationProducer Info");
+        sender.sendMessage("Redis");
+        sender.sendMessage(String.format("Host: %s", plugin.getRedisHost()));
+        sender.sendMessage(String.format("Port: %d", plugin.getRedisPort()));
+        sender.sendMessage(String.format("Channel: %s", plugin.getRedisChannel()));
+    }
+}
