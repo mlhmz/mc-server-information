c7197884810fe25f32fcc81b25d97e0ce377165c
Malek Hamza <malek.hamza@gmx.de>
2023-08-30 20:18:08 +0200

    Improve nav styling

--- server-information-ui/src/components/Nav.tsx
+++ server-information-ui/src/components/Nav.tsx
@@ -1,7 +1,7 @@
 export const Nav = () => {
   return (
-    <nav className="bg-red-400 p-5 border-b-2 border-b-red-200">
-        <h1 className="font-bold text-2xl text-white">Minecraft Server Information</h1>
+    <nav className="bg-red-400 p-4 border-b-2 border-b-red-200">
+        <h1 className="font-bold text-xl text-white">Minecraft Server Information</h1>
     </nav>
   );
 };
