60d3d3a128cd296217fa8e5bb34acf14cf872c1d
Malek Hamza <malek.hamza@gmx.de>
2023-08-30 21:11:03 +0200

    add small search function

--- server-information-ui/src/App.tsx
+++ server-information-ui/src/App.tsx
@@ -1,14 +1,11 @@
-import { useEffect } from "react";
+import { useState } from "react";
 import { Card } from "./components/Card";
 import { Nav } from "./components/Nav";
 import useInformation from "./hooks/use-information";
 
 function App() {
-  const { data, refresh } = useInformation();
-
-  useEffect(() => {
-    refresh();
-  }, [refresh]);
+  const { data } = useInformation();
+  const [searchValue, setSearchValue] = useState("");
 
   return (
     <>
@@ -16,7 +13,21 @@ function App() {
         <Nav />
         <div className="pt-20">
           <div className="flex flex-col items-center md:w-9/12 lg:w-1/2 xl:w-1/3 m-auto">
-            {data.map((information, index) => <Card key={index} information={information} />)}
+            <input
+              value={searchValue}
+              onChange={(e) => setSearchValue(e.target.value)}
+              placeholder="Search for Name"
+              className="bg-stone-800 border border-stone-700 rounded-md py-2 px-2 w-9/12"
+            />
+            {data
+              .filter((information) => {
+                const name = information.name;
+                if (name === undefined) return false;
+                return name.toLowerCase().includes(searchValue.toLowerCase());
+              })
+              .map((information, index) => (
+                <Card key={index} information={information} />
+              ))}
           </div>
         </div>
       </div>
